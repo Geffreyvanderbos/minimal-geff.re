4ab92d5471595fddb322d0e56a9399d6cd629d96
Geffrey <geffrey.bos@gmail.com>
2025-04-30 11:50:30 +0200

    added initial layout of website

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+module.exports = function(eleventyConfig) {
+  // Copy static assets directly to output
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("images");
+
+  return {
+    dir: {
+      input: ".",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_layouts",
+      data: "_data"
+    },
+    templateFormats: ["njk", "md", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk"
+  };
+}; 
